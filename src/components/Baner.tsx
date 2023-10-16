6587f40595d1e104850d24a9d08712f8158505c2
Jakub Centkiewicz <cj50942@zut.edu.pl>
2023-10-16 18:53:46 +0200

    Update the banner component styles (generated by useblackbox.ai )

--- src/components/Baner.tsx
+++ src/components/Baner.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Banner: React.FC = () => {
   return (
-    <div className="bg-orange-600 rounded-lg shadow-md p-4 m-2 min-h-[30vh] flex items-center justify-center ">
+    <div className="bg-gradient-to-br from-indigo-600 via-indigo-700 to-blue-900 rounded-lg shadow-2xl p-6 m-4 min-h-[30vh] flex items-center justify-center transform transition-transform hover:scale-105 hover:shadow-3xl">
       <div className="text-center text-white">
-        <p className="text-3xl font-extrabold">
-          Zadzwoń teraz:{" "}
-          <span className="font-bold text-amber-300">123-456-789</span>
+        <p className="text-4xl font-extrabold">
+          Potrzebujesz więcej informacji?
         </p>
-
-        <p className="text-center mt-4 text-lg">
-          lub napisz:{" "}
-          <span className="font-bold text-amber-300">example@example.com</span>
+        <p className="text-center text-2xl mt-4 ">
+          Skontaktuj się z nami:{" "}
+          <span className="font-bold text-amber-400">+48 530-301-301</span> lub
+          napisz na{" "}
+          <span className="font-bold text-amber-400">example@example.com</span>
         </p>
       </div>
     </div>
