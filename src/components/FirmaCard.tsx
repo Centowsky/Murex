00797fce35083c08696424c2c39e9b0509bd8c8b
Jakub Centkiewicz <cj50942@zut.edu.pl>
2023-10-16 19:25:25 +0200

    Update FirmaCard component styles (generated by useblackbox.ai )

--- src/components/FirmaCard.tsx
+++ src/components/FirmaCard.tsx
@@ -22,15 +22,15 @@ const iconMap: { [key: string]: any } = {
 const FirmaCard: React.FC<FirmaCardProps> = ({ title, content, iconName }) => {
   const IconComponent = iconMap[iconName];
   return (
-    <div className="min-h-[25vh] bg-orange-600 rounded-lg shadow-lg p-4 m-2 md:min-h-[20vh]">
+    <div className="min-h-[25vh] md:min-h-[20vh] bg-orange-600 rounded-lg shadow-lg p-4">
       <div className="flex items-start">
         {IconComponent && <IconComponent className="w-8 h-8 text-white" />}
       </div>
       <div className="flex items-center flex-col justify-center text-center">
-        <p className="text-2xl font-bold text-white">
+        <p className="text-3xl font-bold text-white">
           <span>{title}</span>
         </p>
-        <p className="text-gray-100 mt-4 text-xl">{content}</p>
+        <p className="text-gray-100 text-xl mt-4 ">{content}</p>
       </div>
     </div>
   );
