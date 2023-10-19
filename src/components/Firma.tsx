81d4f7b5a7876a5e51756ed25484ec1693e57fcd
Centowsky <centowsky@gmail.com>
2023-10-19 21:17:49 +0200

    Update the design of the about us page (generated by useblackbox.ai )

--- src/components/Firma.tsx
+++ src/components/Firma.tsx
@@ -13,35 +13,35 @@ const Firma: React.FC = () => {
         </h1>
       </div>
       <div className="flex flex-wrap justify-center p-4 md:w-3/4 mx-auto">
-        <div className="sm:w-full md:w-1/3 p-4">
+        <div className="sm:w-full md:w-1/2 lg:w-1/3 p-4">
           <FirmaCard
             title="12 lat doświadczenia"
             content="Od ponad 12 lat świadczymy usługi na najwyższym poziomie. Dołącz do grona naszych zadowolonych klientów."
             iconName="FaTools"
           />
         </div>
-        <div className="sm:w-full md:w-1/3 p-4">
+        <div className="sm:w-full md:w-1/2 lg:w-1/3 p-4">
           <FirmaCard
             title="Darmowa wycena"
             content="Zadzwoń i skorzystaj z darmowej wyceny. Ustalimy szczegółowy zakres prac oraz termin wykonania usługi."
             iconName="FaMoneyBillWave"
           />
         </div>
-        <div className="sm:w-full md:w-1/3 p-4">
+        <div className="sm:w-full md:w-1/2 lg:w-1/3 p-4">
           <FirmaCard
             title="Szybka realizacja"
             content="Potrzebujesz pilnie oczyścić rynny albo usunąć zagrażające bezpieczeństwu drzewo? Pomożemy od ręki."
             iconName="FaClock"
           />
         </div>
-        <div className="sm:w-full md:w-1/2 p-4">
+        <div className="sm:w-full md:w-1/2 lg:w-1/3 p-4">
           <FirmaCard
             title="Profesjonalny sprzęt i fachowa pomoc"
-            content="Dysponujemy specjalistycznym sprzętem oraz doświadczonym zespołem specjalistów. Pracujemy w trudnym terenie oraz w parkach i na wypielęgnowanych trawnikach nie powodując zniszczeń."
+            content="Dysponujemy specjalistycznym sprzętem oraz doświadczonym zespołem specjalistów. Pracujemy w trudnym terenie oraz w parkach."
             iconName="FaInfoCircle"
           />
         </div>
-        <div className="sm:w-full md:w-1/2 p-4">
+        <div className="sm:w-full md:w-full lg:w-2/3 p-4">
           <Baner />
         </div>
       </div>
--- src/components/FirmaCard.tsx
+++ src/components/FirmaCard.tsx
@@ -22,12 +22,14 @@ const iconMap: { [key: string]: any } = {
 const FirmaCard: React.FC<FirmaCardProps> = ({ title, content, iconName }) => {
   const IconComponent = iconMap[iconName];
   return (
-    <div className="min-h-[30vh] md:min-h-[40vh] lg:md:min-h-[30vh] bg-orange-600 rounded-lg shadow-lg p-4 flex flex-col items-center justify-center text-center relative">
+    <div className="sm:min-h-[50vh] md:min-h-[40vh] lg:md:min-h-[30vh] bg-orange-600 rounded-lg shadow-lg p-4 flex flex-col items-center justify-center text-center relative">
       {IconComponent && (
         <IconComponent className="w-8 h-8 text-white top-2 left-2 absolute" />
       )}{" "}
-      <p className="text-3xl font-bold text-white mt-10 md:mt-4">{title}</p>
-      <p className="text-gray-100 text-2xl mt-4">{content}</p>
+      <p className="text-3xl md:text-2xl lg:text-xl font-bold text-white mt-6">
+        {title}
+      </p>
+      <p className="text-gray-100 text-2xl md:text:xl lg:text-lg">{content}</p>
     </div>
   );
 };
