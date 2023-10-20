54a6f501233138353bf1ffdcc1c31977a17f970d
Centowsky <centowsky@gmail.com>
2023-10-20 19:44:33 +0200

    Update the layout of the Wizytowka component (generated by useblackbox.ai )

--- src/components/Wizytowka.tsx
+++ src/components/Wizytowka.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Wizytowka: React.FC = () => {
   return (
-    <div className="bg-white rounded-lg shadow-lg p-6 w-full flex justify-evenly">
-      <div className="border-2 p-4 border-gray-300 min-h-[20vh] w-1/6">
+    <div className="rounded-lg shadow-lg w-full flex flex-col md:flex-row justify-evenly">
+      <div className=" bg-gray-100 min-h-[20vh] w-screen md:w-1/3">
         <h2 className="text-xl font-bold">MUREX Jarosław Organista</h2>
         <p>Ławy, 74-300 Myślibórz</p>
         <p>
@@ -14,7 +14,7 @@ const Wizytowka: React.FC = () => {
         </p>
         <p>NIP: 597-156-33-173</p>
       </div>
-      <div className="border-2 p-4 border-gray-300 min-h-[20vh] w-1/6">
+      <div className=" bg-gray-100 min-h-[20vh] w-screen md:w-1/3">
         <p className="text-xl font-bold">Usługi</p>
         <ul className="list-disc list-inside ml-4">
           <li>Wycinka i pielęgnacja drzew</li>
@@ -22,7 +22,7 @@ const Wizytowka: React.FC = () => {
           <li>Usługi dźwigowe Stargard</li>
         </ul>
       </div>
-      <div className="border-2 p-4 border-gray-300 min-h-[20vh] w-1/6">
+      <div className=" bg-gray-100 min-h-[20vh] w-screen md:w-1/3">
         <p className="text-xl font-bold">Usługi</p>
         <ul className="list-disc list-inside ml-4">
           <li>Usługi dźwigowe Stargard</li>
